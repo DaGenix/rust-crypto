5460acfd0daa59dc950879126a5c5e94b4f78728
Michael Gehring <mg@ebfe.org>
2014-06-04 17:08:09 +0200

    chacha20: add benchmark

--- src/rust-crypto/chacha20.rs
+++ src/rust-crypto/chacha20.rs
@@ -269,3 +269,43 @@ mod test {
         }
     }
 }
+
+#[cfg(test)]
+mod bench {
+    use test::Bencher;
+    use symmetriccipher::SynchronousStreamCipher;
+    use chacha20::ChaCha20;
+
+    #[bench]
+    pub fn chacha20_10(bh: & mut Bencher) {
+        let mut chacha20 = ChaCha20::new([0, ..32], [0, ..8]);
+        let input = [1u8, ..10];
+        let mut output = [0u8, ..10];
+        bh.iter( || {
+            chacha20.process(input, output);
+        });
+        bh.bytes = input.len() as u64;
+    }
+
+    #[bench]
+    pub fn chacha20_1k(bh: & mut Bencher) {
+        let mut chacha20 = ChaCha20::new([0, ..32], [0, ..8]);
+        let input = [1u8, ..1024];
+        let mut output = [0u8, ..1024];
+        bh.iter( || {
+            chacha20.process(input, output);
+        });
+        bh.bytes = input.len() as u64;
+    }
+
+    #[bench]
+    pub fn chacha20_64k(bh: & mut Bencher) {
+        let mut chacha20 = ChaCha20::new([0, ..32], [0, ..8]);
+        let input = [1u8, ..65536];
+        let mut output = [0u8, ..65536];
+        bh.iter( || {
+            chacha20.process(input, output);
+        });
+        bh.bytes = input.len() as u64;
+    }
+}
