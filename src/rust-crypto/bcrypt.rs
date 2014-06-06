c69a2a0bca04886f26df3df429ecafd9f40beed8
Michael Gehring <mg@ebfe.org>
2014-06-06 02:13:34 +0200

    bcrypt: add simple benchmark

--- src/rust-crypto/bcrypt.rs
+++ src/rust-crypto/bcrypt.rs
@@ -148,3 +148,19 @@ mod test {
         }
     }
 }
+
+#[cfg(test)]
+mod bench {
+    use test::Bencher;
+    use bcrypt::bcrypt;
+
+    #[bench]
+    pub fn bcrypt_16_5(bh: & mut Bencher) {
+        let pass = [0u8, ..16];
+        let salt = [0u8, ..16];
+        let mut out  = [0u8, ..24];
+        bh.iter( || {
+            bcrypt(5, salt, pass, out);
+        });
+    }
+}
