7c38710512ebcde80e0ce524a82205b5cfc11d56
Palmer Cox <p@lmercox.com>
2014-06-07 21:10:08 -0400

    Re-write supports_aesni() function to compile on 32-bit x86

--- src/rust-crypto/util.rs
+++ src/rust-crypto/util.rs
@@ -4,38 +4,26 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// This should go in either 'sys' or 'os'
-#[cfg(target_arch = "x86")]
-#[cfg(target_arch = "x86_64")]
-#[allow(dead_assignment)]
-fn cpuid(func: u32) -> (u32, u32, u32, u32) {
-    let mut a = 0u32;
-    let mut b = 0u32;
-    let mut c = 0u32;
-    let mut d = 0u32;
-
-    unsafe {
-        asm!(
-        "
-        mov $4, %eax;
-        cpuid;
-        mov %eax, $0;
-        mov %ebx, $1;
-        mov %ecx, $2;
-        mov %edx, $3;
-        "
-        : "=r" (a), "=r" (b), "=r" (c), "=r" (d)
-        : "r" (func)
-        : "eax", "ebx", "ecx", "edx"
-        )
-    }
-
-    return (a, b, c, d);
-}
-
 #[cfg(target_arch = "x86")]
 #[cfg(target_arch = "x86_64")]
 pub fn supports_aesni() -> bool {
-    let (_, _, c, _) = cpuid(1);
-    return (c & 0x02000000) != 0;
+	let mut flags: u32;
+    unsafe {
+        asm!(
+        "
+        mov $$1, %eax;
+        cpuid;
+		mov %ecx, $0;
+        "
+        : "=r" (flags) // output
+        : // input
+        : "eax", "ebx", "ecx", "edx" // clobbers
+        )
+		// No idea why, but on 32-bit targets, the compiler complains
+		// about not having enough registers. Adding in this dummy
+		// section, however, seems to fix it.
+        asm!("")
+    }
+
+    return (flags & 0x02000000) != 0;
 }
