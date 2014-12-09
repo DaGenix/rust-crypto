8fd99379294e9f912606669556ea9b8f8a951267
Calvin Metcalf <cmetcalf@appgeo.com>
2014-12-09 12:30:37 -0500

    rm unneeded experimental flag

--- src/rust-crypto/chacha20.rs
+++ src/rust-crypto/chacha20.rs
@@ -3,7 +3,6 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
-#![allow(experimental)]
 use std::simd::u32x4;
 
 use buffer::{BufferResult, RefReadBuffer, RefWriteBuffer};
