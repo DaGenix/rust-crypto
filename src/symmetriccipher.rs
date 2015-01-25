c62ac1652301a3a8bcf2f39296df62cdb66a1312
Corey Richardson <corey@octayn.net>
2015-01-25 07:24:24 -0500

    Derive Debug on SymmetricCipherError

--- src/symmetriccipher.rs
+++ src/symmetriccipher.rs
@@ -27,7 +27,7 @@ pub trait BlockDecryptorX8 {
     fn decrypt_block_x8(&self, input: &[u8], output: &mut [u8]);
 }
 
-#[derive(Copy)]
+#[derive(Debug, Copy)]
 pub enum SymmetricCipherError {
     InvalidLength,
     InvalidPadding
