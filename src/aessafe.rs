6bf114742c836b5043f1e3eb84191e91a1429535
Palmer Cox <p@lmercox.com>
2015-04-03 01:36:44 -0400

    Remove use of SIMD types, for now
    The simd module should be able to support using real SIMD types
    when compiled with a nightly and 'fake' SIMD types when compiled
    against stable. However, I'm hitting some ICE that I don't know
    how to workaround when trying to use the real SIMD types with this
    approach. So, disable SIMD types until we can figure that out.

--- src/aessafe.rs
+++ src/aessafe.rs
@@ -127,16 +127,9 @@ use std::ops::{BitAnd, BitXor, Not};
 use std::default::Default;
 
 use cryptoutil::{read_u32v_le, write_u32_le};
+use simd::u32x4;
 use step_by::RangeExt;
 use symmetriccipher::{BlockEncryptor, BlockEncryptorX8, BlockDecryptor, BlockDecryptorX8};
-
-// Using std::unstable::simd::u32x4 results in issues creating static arrays of u32x4 values.
-// Defining the type here avoids that problem. Additionally, we need to implement various trait from
-// libstd which wouldn't be possible if we used that type directly.
-#[simd]
-#[derive(Copy, Eq, PartialEq)]
-#[allow(non_camel_case_types)]
-pub struct u32x4(u32, u32, u32, u32);
 
 const U32X4_0: u32x4 = u32x4(0, 0, 0, 0);
 const U32X4_1: u32x4 = u32x4(-1, -1, -1, -1);
@@ -1193,22 +1186,6 @@ impl u32x4 {
             (a1 >> s) | (a2 << (32 - s)),
             (a2 >> s) | (a3 << (32 - s)),
             a3 >> s)
-    }
-}
-
-impl BitXor for u32x4 {
-    type Output = u32x4;
-
-    fn bitxor(self, rhs: u32x4) -> u32x4 {
-        self ^ rhs
-    }
-}
-
-impl BitAnd for u32x4 {
-    type Output = u32x4;
-
-    fn bitand(self, rhs: u32x4) -> u32x4 {
-        self & rhs
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![feature(simd)]
 #![cfg_attr(test, feature(test))]
 
 extern crate rand;
