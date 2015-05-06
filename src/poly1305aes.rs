12832db39e1adc9841bdf2d5a5f271bd217e66d8
Jason Ozias <jason.g.ozias@gmail.com>
2015-05-06 18:50:37 -0400

    poly1305aes work

--- src/poly1305aes.rs
+++ src/poly1305aes.rs
@@ -8,11 +8,21 @@ pub fn poly1305aes_clamp(kr: &mut [u8; 32]) {
     kr[28] = kr[28] & 252; // r[13]
 }
 
-pub fn poly1305aes_authenticate(kr: [u8; 32], n: [u8; 16]
+pub fn poly1305aes_authenticate(kr: [u8; 32],
+                                n: [u8; 16],
+                                _m: &[u8]) -> [u8; 16] {
+    let k = &kr[0..16];
+    let r = &kr[16..32];
+
+    println!("k: {:?}", k);
+    println!("r: {:?}", r);
+    println!("n: {:?}", n);
+    [0; 16]
+}
 
 #[cfg(test)]
 mod test {
-    use poly1305aes::poly1305aes_clamp;
+    use poly1305aes::{poly1305aes_authenticate,poly1305aes_clamp};
     use rand::{OsRng,Rng};
 
     fn bottom_two_bits_zeroed(val: u8) -> bool {
@@ -41,5 +51,19 @@ mod test {
         assert!(top_four_bits_zeroed(k[23]));
         assert!(bottom_two_bits_zeroed(k[24]));
         assert!(bottom_two_bits_zeroed(k[28]));
+    }
+
+    #[test]
+    fn test_authenticate() {
+        let mut k: [u8; 32] = [0; 32];
+        let m = b"This is a test";
+        let mut rng = match OsRng::new() {
+            Ok(rng) => rng,
+            Err(e)  => panic!("Failed to create rng! {}", e),
+        };
+
+        rng.fill_bytes(&mut k);
+        poly1305aes_clamp(&mut k);
+        let _ = poly1305aes_authenticate(k, [0; 16], &m[..]);
     }
 }
