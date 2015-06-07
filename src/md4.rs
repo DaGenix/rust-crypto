2f4bbf583f593185a0ff7d4c13ad3e657d1e772f
Slava Bacherikov <slava@bacher09.org>
2015-06-07 17:10:44 +0300

    Add benchmarks for md4

--- src/md4.rs
+++ src/md4.rs
@@ -233,3 +233,41 @@ mod tests {
             "bbce80cc6bb65e5c6745e30d4eeca9a4");
     }
 }
+
+
+#[cfg(all(test, feature = "with-bench"))]
+mod bench {
+    use test::Bencher;
+    use digest::Digest;
+    use md4::Md4;
+
+    #[bench]
+    pub fn md4_10(bh: & mut Bencher) {
+        let mut sh = Md4::new();
+        let bytes = [1u8; 10];
+        bh.iter( || {
+            sh.input(&bytes);
+        });
+        bh.bytes = bytes.len() as u64;
+    }
+
+    #[bench]
+    pub fn md4_1k(bh: & mut Bencher) {
+        let mut sh = Md4::new();
+        let bytes = [1u8; 1024];
+        bh.iter( || {
+            sh.input(&bytes);
+        });
+        bh.bytes = bytes.len() as u64;
+    }
+
+    #[bench]
+    pub fn md4_64k(bh: & mut Bencher) {
+        let mut sh = Md4::new();
+        let bytes = [1u8; 65536];
+        bh.iter( || {
+            sh.input(&bytes);
+        });
+        bh.bytes = bytes.len() as u64;
+    }
+}
