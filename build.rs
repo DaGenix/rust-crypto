410dcf710d16d7719e4ba7f374aef899842dfa05
Palmer Cox <palmer@fiscalnote.com>
2016-05-07 02:00:12 -0400

    Add a feature flag to omit ASM dependencies
    Compiling the ASM dependencies has turned out to be a major pain point.
    This commit adds a feature flag to optionally disable support for all
    algorithms that require ASM.

--- build.rs
+++ build.rs
@@ -10,32 +10,33 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-    let target = env::var("TARGET").unwrap();
-    let host = env::var("HOST").unwrap();
-    if target.contains("msvc") && host.contains("windows") {
-        let mut config = gcc::Config::new();
-        config.file("src/util_helpers.asm");
-        config.file("src/aesni_helpers.asm");
-        if target.contains("x86_64") {
-            config.define("X64", None);
-        }
-        config.compile("lib_rust_crypto_helpers.a");
-    }
-    else {
-        let mut cfg = gcc::Config::new();
-        cfg.file("src/util_helpers.c");
-        cfg.file("src/aesni_helpers.c");
-        if env::var_os("CC").is_none() {
-            if host.contains("openbsd") {
-                // Use clang on openbsd since there have been reports that
-                // GCC doesn't like some of the assembly that we use on that
-                // platform.
-                cfg.compiler(Path::new("clang"));
-            } else if target == host {
-                cfg.compiler(Path::new("cc"));
+    if env::var("CARGO_FEATURE_WITH_ASM").is_ok() {
+        let target = env::var("TARGET").unwrap();
+        let host = env::var("HOST").unwrap();
+        if target.contains("msvc") && host.contains("windows") {
+            let mut config = gcc::Config::new();
+            config.file("src/util_helpers.asm");
+            config.file("src/aesni_helpers.asm");
+            if target.contains("x86_64") {
+                config.define("X64", None);
             }
+            config.compile("lib_rust_crypto_helpers.a");
         }
-        cfg.compile("lib_rust_crypto_helpers.a");
+        else {
+            let mut cfg = gcc::Config::new();
+            cfg.file("src/util_helpers.c");
+            cfg.file("src/aesni_helpers.c");
+            if env::var_os("CC").is_none() {
+                if host.contains("openbsd") {
+                    // Use clang on openbsd since there have been reports that
+                    // GCC doesn't like some of the assembly that we use on that
+                    // platform.
+                    cfg.compiler(Path::new("clang"));
+                } else if target == host {
+                    cfg.compiler(Path::new("cc"));
+                }
+            }
+            cfg.compile("lib_rust_crypto_helpers.a");
+        }
     }
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -16,33 +16,46 @@ extern crate test;
 
 pub mod aead;
 pub mod aes;
+#[cfg(feature = "with-asm")]
 pub mod aes_gcm;
 pub mod aessafe;
 pub mod bcrypt;
 pub mod bcrypt_pbkdf;
+#[cfg(feature = "with-asm")]
 pub mod blake2b;
+#[cfg(feature = "with-asm")]
 pub mod blake2s;
 pub mod blockmodes;
 pub mod blowfish;
 pub mod buffer;
 pub mod chacha20;
+#[cfg(feature = "with-asm")]
 pub mod chacha20poly1305;
 mod cryptoutil;
+#[cfg(feature = "with-asm")]
 pub mod curve25519;
 pub mod digest;
+#[cfg(feature = "with-asm")]
 pub mod ed25519;
 pub mod fortuna;
+#[cfg(feature = "with-asm")]
 pub mod ghash;
 pub mod hc128;
+#[cfg(feature = "with-asm")]
 pub mod hmac;
+#[cfg(feature = "with-asm")]
 pub mod hkdf;
+#[cfg(feature = "with-asm")]
 pub mod mac;
 pub mod md5;
+#[cfg(feature = "with-asm")]
 pub mod pbkdf2;
+#[cfg(feature = "with-asm")]
 pub mod poly1305;
 pub mod rc4;
 pub mod ripemd160;
 pub mod salsa20;
+#[cfg(feature = "with-asm")]
 pub mod scrypt;
 pub mod sha1;
 pub mod sha2;
@@ -51,8 +64,9 @@ mod simd;
 pub mod sosemanuk;
 mod step_by;
 pub mod symmetriccipher;
+#[cfg(feature = "with-asm")]
 pub mod util;
 pub mod whirlpool;
 
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[cfg(all(feature = "with-asm", any(target_arch = "x86", target_arch = "x86_64")))]
 pub mod aesni;
